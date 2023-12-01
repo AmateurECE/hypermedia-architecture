01021a1e224d25d74e827e69b6b6d3ff71005d1f
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-11-30 21:39:25 -0600

    Create icg-server crate and tests

---
+++ icg-server/tests/domain.rs
@@ -0,0 +1,15 @@
+
+#[test]
+fn req1_variance_calculation() {
+    unimplemented!()
+}
+
+#[test]
+fn req2_station_health_status() {
+    unimplemented!()
+}
+
+#[test]
+fn req3_station_unhealthy_status() {
+    unimplemented!()
+}
